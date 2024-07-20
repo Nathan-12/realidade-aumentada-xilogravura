348c24bb309131b08564626206f176870ada51bb
Nathan Lima Batista <nathanlima.b@gmail.com>
2024-07-20 18:22:57 -0300

    feat: adicionado os videos estaticos

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+    
+    
+
+    function closeModal() {
+        const modal = document.getElementById("myModal");
+        modal.style.display = "none";
+    }
+
+    // Fechar o modal ao clicar fora dele
+    window.onclick = function(event) {
+        const modal = document.getElementById("myModal");
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    function openModal() {
+      var modal = document.getElementById('modal-media-video');
+      var video = document.getElementById('video-content');
+      modal.style.display = 'block';
+      video.play(); // Inicia o vídeo assim que o modal é aberto
+  }
+
+  function closeModal() {
+      var modal = document.getElementById('modal-media-video');
+      var video = document.getElementById('video-content');
+      video.pause();
+      video.currentTime = 0;
+      modal.style.display = 'none';
+  }
+
+  // Fechar o modal ao clicar fora do vídeo
+  document.addEventListener('click', function(event) {
+      var modal = document.getElementById('modal-media-video');
+      if (event.target === modal) {
+          closeModal();
+      }
+  });
+
+  
